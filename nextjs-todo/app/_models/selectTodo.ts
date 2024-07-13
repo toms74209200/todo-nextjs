d44b84d73c005a671cbf43bfd06697fbd77e8281
toms74209200 <motomotomato@gmail.com>
2024-07-13 20:12:34 +0900

    :sparkles: Update todo

--- nextjs-todo/app/_models/selectTodo.ts
+++ nextjs-todo/app/_models/selectTodo.ts
@@ -7,16 +7,15 @@ export const selectTodo = async () => {
   const firestore = await getFirestoreAdmin();
 
   const snapshot = await firestore.collection("todos").get();
-  const todos: Todo[] = snapshot.docs
-    .map((doc) => doc.data())
-    .map((data) => {
-      return {
-        title: data.title,
-        description: data.description,
-        deadline: data.deadline,
-        completed: data.completed,
-      };
-    });
+  const todos: Todo[] = snapshot.docs.map((doc) => {
+    return {
+      id: doc.id,
+      title: doc.data().title,
+      description: doc.data().description,
+      deadline: doc.data().deadline,
+      completed: doc.data().completed,
+    };
+  });
 
   return todos;
 };
