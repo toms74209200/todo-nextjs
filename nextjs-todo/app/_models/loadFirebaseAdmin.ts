2bd7de903ca5bb3c69a26f45a43b62e5f8a6019c
toms74209200 <motomotomato@gmail.com>
2024-07-27 15:41:08 +0900

    :sparkles: Add Firebase Auth Admin

--- nextjs-todo/app/_models/loadFirebaseAdmin.ts
+++ nextjs-todo/app/_models/loadFirebaseAdmin.ts
@@ -1,12 +1,18 @@
 "use server";
 import { initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
 const app = initializeApp({
   projectId: "nextjs-todo",
 });
 const firestore = getFirestore();
+const auth = getAuth();
 
 export const getFirestoreAdmin = async () => {
   return firestore;
+};
+
+export const getAuthAdmin = async () => {
+  return auth;
 };
